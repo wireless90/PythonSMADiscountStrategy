f6717d10602229884f3541298383e04e2dbf8a38
Muhammad Razali <wireless-@hotmail.com>
2019-10-28 20:42:03 +0800

    Added class comments

--- PythonSMADiscountTrading/CredentialService.py
+++ PythonSMADiscountTrading/CredentialService.py
@@ -1,4 +1,9 @@
 class CredentialService:
+    '''
+    This class contains all the credentials used by the program.
+    Please do not upload your credentials to any websites.
+    '''
+
     API_KEY = 'API KEY HERE'
     API_SECRET = 'API SECRET HERE'
     EMAIL_ADD = 'EMAIL ADDRESS HERE'
