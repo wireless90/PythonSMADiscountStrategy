fcc75cc7a6bc07658afb5e21e8f448a60786604c
Muhammad Razali <wireless-@hotmail.com>
2021-08-28 22:57:52 +0800

    added self.sma

--- PythonSMADiscountTrading/BacktesterService.py
+++ PythonSMADiscountTrading/BacktesterService.py
@@ -21,13 +21,14 @@ class BacktesterService:
         self.profit_target_1 = (1 + profit_target_1) / 100
         self.profit_target_2 = (1 + profit_target_2) / 100
         self.cursor = 0
+        self.sma = 0.0;
     
     def start(self): 
-        if self.__has_enough_candlesticks() == False:
-            return 0
-
-        while self.cursor < len(self.candlesticks):
+        while self.sma != -1:
             sma = self.__compute_sma()
+
+            # Code here
+
             self.cursor += 1
 
 
@@ -45,4 +46,4 @@ class BacktesterService:
             return -1;
 
         candlesticks = self.candlesticks[self.cursor:(self.cursor+self.sma_period)]
-        return sum([candlestick.close for candlestick in candlesticks]) / self.sma_period
+        self.sma = sum([candlestick.close for candlestick in candlesticks]) / self.sma_period
