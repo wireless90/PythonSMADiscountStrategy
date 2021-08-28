754cd59332219d87aa097799fb5c531313cd0cdc
Muhammad Razali <wireless-@hotmail.com>
2021-08-28 22:04:17 +0800

    reworked __compute_sma and __has_enough_candlesticks

--- PythonSMADiscountTrading/BacktesterService.py
+++ PythonSMADiscountTrading/BacktesterService.py
@@ -14,6 +14,7 @@ class BacktesterService:
     PROFIT_TARGET_2 = 5
 
     def __init__(self, candlesticks, sma_period=BacktesterService.SMA_PERIOD, buy_target_1=BacktesterService.BUY_TARGET_1, buy_target_2=BacktesterService.BUY_TARGET_2, profit_target_1=BacktesterService.PROFIT_TARGET_1, profit_target_2=BacktesterService.PROFIT_TARGET_2):
+        self.candlesticks = candlesticks
         self.sma_period = sma_period
         self.buy_target_1 = (1 - buy_target_1) / 100
         self.buy_target_2 = (1 - buy_target_2) / 100
@@ -35,8 +36,13 @@ class BacktesterService:
     # private functions
 
     def __has_enough_candlesticks(self):
-        return len(self.candlesticks) > self.sma_period
+        candlesticks_left = len(self.candlesticks) - cursor
+        return candlesticks_left > self.sma_period
     
 
     def __compute_sma(self):
-        sum([candlestick.close for candlestick in self.candlesticks]) / BacktesterService.SMA_PERIOD
+        if __has_enough_candlesticks() == False:
+            return -1;
+
+        candlesticks = self.candlesticks[self.cursor:(self.cursor+self.sma_period)]
+        return sum([candlestick.close for candlestick in candlesticks]) / self.sma_period
